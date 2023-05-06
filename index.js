0d183ab6e8cf0931719735614ec6f8d81ce1aad1
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-05-06 17:16:45 -0500

    Completed Lab.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const initBatt = 0;
+const totalBatteries = batteryBatches.reduce(
+    (accumulator, totalNow) => accumulator + totalNow,
+    initBatt
+);
+totalBatteries
